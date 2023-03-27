dad154db7c69cf8fd1711fb3fbc2bd025a55f97b
soybean15 <marlonpadilla1593@gmail.com>
2023-03-27 14:44:36 +0800

    added recommendation view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    
-
-  </div>
+  <RecommendationView/>
+  
 </template>
 
 <script>
+
+import RecommendationView from '../views/components/homepage-components/RecommendationView.vue'
+
 // @ is an alias to /src
 
-
 export default {
-  name: 'HomeView',
-  components: {
-  
-  }
-}
+  name: "HomeView",
+  components: {RecommendationView},
+
+};
 </script>
+
+<style>
+
+</style>
