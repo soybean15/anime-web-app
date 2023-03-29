72a4d5dd5498f697792520796b8f6158a1dd0a96
soybean15 <marlonpadilla1593@gmail.com>
2023-03-29 23:25:27 +0800

    done dark mode

--- src/views/components/other/DarkModeToggle.vue
+++ src/views/components/other/DarkModeToggle.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="dark-mode-toggle h-5 w-5 pl-0.5">
-        <div v-if="darkMode">
+        <div v-if="!darkMode">
             <img src="../../../assets/icons/sun.png">
         </div>
         <div v-else>
