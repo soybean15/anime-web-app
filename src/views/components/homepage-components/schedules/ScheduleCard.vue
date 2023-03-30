dbdf4d2e5ace01ede96cdb04d70c3039ec5b3d74
soybean15 <marlonpadilla1593@gmail.com>
2023-03-30 22:23:34 +0800

    added schedule card

---
+++ src/views/components/homepage-components/schedules/ScheduleCard.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <div class="flex h-40 m-3 border-solid  border border-gray-700  bg-slate-800 bg-zinc-800 rounded-lg">
+        <img class="rounded-l-lg sched-img w-2/5  "  :src=" schedule.images.jpg.image_url">
+     
+        <div class="w-9/12 ">
+            <div class="text-left p-1.5">{{schedule.title}}</div>
+            <div class="text-left p-1.5">{{ schedule.broadcast.day}} : {{ schedule.broadcast.time }} </div>
+            <div class="text-left p-1.5">{{ schedule.type}} </div>
+            <div class="text-left p-1.5">{{ schedule.synopsis}} </div>
+
+        </div>
+
+
+    </div>
+  
+</template>
+
+<script>
+export default {
+
+    props: ['schedule']
+}
+</script>
+
+<style>
+.sched-img{
+    object-fit: cover;
+
+
+   object-position:50% 20%;
+}
+</style>
--- src/views/components/homepage-components/schedules/ScheduleView.vue
+++ src/views/components/homepage-components/schedules/ScheduleView.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="title">Schedules</div>
-  <div class="grid grid-cols-1 md:grid-cols-2">
+  <div class="grid grid-cols-1 md:grid-cols-2 h-96 h-fit overflow-auto">
 
     <div v-for="schedule in schedules" :key="schedule.mal_id">
-       {{ schedule.title }} 
+        <ScheduleCard :schedule="schedule"/>
+    
     </div>
 
 
@@ -15,8 +16,9 @@
 <script>
 import {ref} from 'vue'
 import getSchedules from '../../../../data/jikan-api/getSchedules'
+import ScheduleCard from './ScheduleCard.vue'
 export default {
-
+    components:{ScheduleCard},
     setup(){
         const schedules = ref(getSchedules())
         console.log(schedules)
